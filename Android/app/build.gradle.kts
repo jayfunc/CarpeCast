0b6e6a3e97da32246425ab183c811caaabf4c628
jayfunc <founchoo@outlook.com>
2026-08-23 07:38:09 +0800

    chore: bump version to 1.0.1

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1"
         
         buildConfigField("String", "GIT_DATE", "\"${gitDate}\"")
         buildConfigField("String", "GIT_HASH", "\"${gitHash}\"")
